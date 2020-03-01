e3e24f6d5cfab24c63f5dd1873a31539c7aa47ab
Aoífe Gildernew <a.gildernew@kainos.com>
2020-03-01 15:34:42 +0000

    Initial Commit

---
+++ IrishLanguageApplicationDB/ChoosingExerciseForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IrishLanguageApplicationDB
+{
+    public partial class ChoosingExerciseForm : Form
+    {
+        public ChoosingExerciseForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ChoosingExerciseForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void ChoosingExerciseForm_Closing(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
