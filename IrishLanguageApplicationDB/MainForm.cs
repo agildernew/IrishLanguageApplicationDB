862354e06b7cff827a6a830997bca4dc710844dd
Aoífe Gildernew <a.gildernew@kainos.com>
2020-03-01 11:48:44 +0000

    MainForm Load

--- IrishLanguageApplicationDB/MainForm.cs
+++ IrishLanguageApplicationDB/MainForm.cs
@@ -20,29 +20,47 @@ namespace IrishLanguageApplicationDB
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            lbxVocabulary.ClearSelected();
+            string selectedTopicNameEnglish = "";
+            string selectedVocabularyIrish= "";
+            string selectedVocabularyEnglish = "";
+
             SqlConnection connection = new SqlConnection();
             connection.ConnectionString = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename=\"C:\\Users\\Ryan Skillen\\Documents\\GitHub\\IrishLanguageApplicationDB\\IrishLanguageApplicationDB\\IrishAppDB.mdf\"; Integrated Security = True";
             
             connection.Open();
             SqlCommand cmd = new SqlCommand("SELECT * FROM Topics;", connection);
             SqlDataReader reader = cmd.ExecuteReader();
+            cbxTopicList.Items.Clear();
             while (reader.Read())
             {
-                cbxTopicList.Items.Add(reader["topic_name_english"].ToString() + " - " + reader["topic_name_irish"]);
+                cbxTopicList.Items.Add(reader["topic_name_english"].ToString() + " - " + reader["topic_name_irish"].ToString());
             }
+            cbxTopicList.SelectedIndex = 0;
+            int index = cbxTopicList.SelectedItem.ToString().IndexOf('-');
+            if (index > 0)
+            {
+                selectedTopicNameEnglish = cbxTopicList.SelectedItem.ToString().Substring(0, index);
+            };
+            txtIrishVocabulary.Text = selectedTopicNameEnglish;
             connection.Close(); 
             
             connection.Open();
-            cmd = new SqlCommand("SELECT * FROM Vocabulary WHERE topic_name_english='Numbers';", connection);
+            cmd = new SqlCommand("SELECT * FROM Vocabulary WHERE topic_name_english='" + selectedTopicNameEnglish + "';", connection);
             reader = cmd.ExecuteReader();
             while (reader.Read())
             {
-                lbxVocabulary.Items.Add(reader["vocabulary_english"].ToString() + " - " + reader["vocabulary_irish"]);
+                lbxVocabulary.Items.Add(reader["vocabulary_english"].ToString() + " - " + reader["vocabulary_irish"].ToString());
             }
+            lbxVocabulary.SelectedIndex = 0;
+            index = lbxVocabulary.SelectedItem.ToString().IndexOf('-');
+            if (index > 0)
+            {
+                selectedVocabularyIrish = lbxVocabulary.SelectedItem.ToString().Substring(index+2, index);
+                selectedVocabularyEnglish = lbxVocabulary.SelectedItem.ToString().Substring(0, index);
+            };
+            txtIrishVocabulary.Text = selectedVocabularyIrish;
+            txtEnglishVocabulary.Text = selectedVocabularyEnglish;
             connection.Close();
-
-            //txtEnglishVocabulary.Text = lbxVocabulary.SelectedItem.ToString();
         }
     }
 }
