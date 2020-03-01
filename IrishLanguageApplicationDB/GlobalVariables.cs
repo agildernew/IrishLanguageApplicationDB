61e0bc55851ba92f54d7a73a04527020bb657339
Aoífe Gildernew <a.gildernew@kainos.com>
2020-03-01 15:09:32 +0000

    Initial Changes

---
+++ IrishLanguageApplicationDB/GlobalVariables.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace IrishLanguageApplicationDB
+{
+    class GlobalVariables
+    {
+        public static SqlConnection SqlConnection()
+        {
+            SqlConnection connection = new SqlConnection();
+            //connection.ConnectionString = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename=\"C:\\Users\\Ryan Skillen\\Documents\\GitHub\\IrishLanguageApplicationDB\\IrishLanguageApplicationDB\\IrishAppDB.mdf\"; Integrated Security = True";
+            return connection;
+        }
+
+        public static string SqlConnectionString()
+        {
+            return null;
+        }
+        
+    }
+
+
+
+
+}
