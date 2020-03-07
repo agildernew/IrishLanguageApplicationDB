1c31d39de7da1d979047e7664e409cc73189d0ee
Aoífe Gildernew <a.gildernew@kainos.com>
2020-03-07 18:57:16 +0000

    Adding Initial Code

---
+++ IrishLanguageApplicationDB/AddTopicsForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IrishLanguageApplicationDB
+{
+    public partial class AddTopicsForm : Form
+    {
+        public AddTopicsForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
