2af55114f93f47118e9c3f5a03247e3ab7e94002
Aoífe Gildernew <a.gildernew@kainos.com>
2020-03-29 16:09:47 +0100

    Multipls Choice Form

---
+++ IrishLanguageApplicationDB/MultipleChoiceForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace IrishLanguageApplicationDB
+{
+    partial class MultipleChoiceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.btnAnswer1 = new System.Windows.Forms.Button();
+            this.btnAnswer2 = new System.Windows.Forms.Button();
+            this.btnAnswer3 = new System.Windows.Forms.Button();
+            this.btnAnswer4 = new System.Windows.Forms.Button();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(13, 13);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(59, 13);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "lblQuestion";
+            // 
+            // btnAnswer1
+            // 
+            this.btnAnswer1.Location = new System.Drawing.Point(16, 43);
+            this.btnAnswer1.Name = "btnAnswer1";
+            this.btnAnswer1.Size = new System.Drawing.Size(111, 23);
+            this.btnAnswer1.TabIndex = 1;
+            this.btnAnswer1.Text = "Answer 1";
+            this.btnAnswer1.UseVisualStyleBackColor = true;
+            // 
+            // btnAnswer2
+            // 
+            this.btnAnswer2.Location = new System.Drawing.Point(137, 43);
+            this.btnAnswer2.Name = "btnAnswer2";
+            this.btnAnswer2.Size = new System.Drawing.Size(111, 23);
+            this.btnAnswer2.TabIndex = 2;
+            this.btnAnswer2.Text = "Answer 2";
+            this.btnAnswer2.UseVisualStyleBackColor = true;
+            // 
+            // btnAnswer3
+            // 
+            this.btnAnswer3.Location = new System.Drawing.Point(16, 72);
+            this.btnAnswer3.Name = "btnAnswer3";
+            this.btnAnswer3.Size = new System.Drawing.Size(111, 23);
+            this.btnAnswer3.TabIndex = 3;
+            this.btnAnswer3.Text = "Answer 3";
+            this.btnAnswer3.UseVisualStyleBackColor = true;
+            // 
+            // btnAnswer4
+            // 
+            this.btnAnswer4.Location = new System.Drawing.Point(137, 72);
+            this.btnAnswer4.Name = "btnAnswer4";
+            this.btnAnswer4.Size = new System.Drawing.Size(111, 23);
+            this.btnAnswer4.TabIndex = 4;
+            this.btnAnswer4.Text = "Answer 4";
+            this.btnAnswer4.UseVisualStyleBackColor = true;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(179, 101);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(69, 22);
+            this.btnConfirm.TabIndex = 5;
+            this.btnConfirm.Text = "Confirm";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            // 
+            // MultipleChoiceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 135);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.btnAnswer4);
+            this.Controls.Add(this.btnAnswer3);
+            this.Controls.Add(this.btnAnswer2);
+            this.Controls.Add(this.btnAnswer1);
+            this.Controls.Add(this.lblQuestion);
+            this.Name = "MultipleChoiceForm";
+            this.Text = "MultipleChoiceForm";
+            this.Load += new System.EventHandler(this.MultipleChoiceForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Button btnAnswer1;
+        private System.Windows.Forms.Button btnAnswer2;
+        private System.Windows.Forms.Button btnAnswer3;
+        private System.Windows.Forms.Button btnAnswer4;
+        private System.Windows.Forms.Button btnConfirm;
+    }
+}
---
+++ IrishLanguageApplicationDB/MultipleChoiceForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace IrishLanguageApplicationDB
+{
+    public partial class MultipleChoiceForm : Form
+    {
+        SqlConnection connection = new SqlConnection();
+        string exerciseTopic = "";
+        List<string> vocabularyEnglish = new List<string>(), vocabularyIrish = new List<string>();
+        string[] sortedVocabularyIrish, sortedVocabularyEnglish;
+        int numberOfInstances = 0;
+
+        public MultipleChoiceForm(string topic)
+        {
+            exerciseTopic = topic;
+
+            InitializeComponent();
+        }
+
+        private void MultipleChoiceForm_Load(object sender, EventArgs e)
+        {
+            connection.ConnectionString = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename=\"C:\\Users\\Ryan Skillen\\Documents\\GitHub\\IrishLanguageApplicationDB\\IrishLanguageApplicationDB\\IrishAppDB.mdf\"; Integrated Security = True";
+
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Vocabulary WHERE topic_name_english='" + exerciseTopic + "';", connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                int index = 0;
+                vocabularyEnglish.Add(reader["vocabulary_english"].ToString());
+                vocabularyIrish.Add(reader["vocabulary_irish"].ToString());
+                index = index + 1;
+            }
+            connection.Close();
+
+            sortedVocabularyEnglish = vocabularyEnglish.ToArray();
+            sortedVocabularyIrish = vocabularyIrish.ToArray();
+
+            numberOfInstances = vocabularyEnglish.Count();
+            Random rand = new Random();
+
+            // http://csharphelper.com/blog/2014/07/randomize-arrays-in-c/
+            for (int i = 0; i < numberOfInstances - 1; i++)
+            {
+                int j = rand.Next(i, numberOfInstances);
+                string temp = sortedVocabularyEnglish[i];
+                sortedVocabularyEnglish[i] = sortedVocabularyEnglish[j];
+                sortedVocabularyEnglish[j] = temp;
+            }
+
+            for (int i = 0; i < numberOfInstances - 1; i++)
+            {
+                int j = rand.Next(i, numberOfInstances);
+                string temp = sortedVocabularyIrish[i];
+                sortedVocabularyIrish[i] = sortedVocabularyIrish[j];
+                sortedVocabularyIrish[j] = temp;
+            }
+        }
+    }
+}
